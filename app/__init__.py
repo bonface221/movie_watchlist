b7ac85b2d57b7b408ad2db50d354ea3bd929e8b4
[bonface221] <[mainaboniface221@gmail.com]>
2022-04-27 07:54:49 +0300

    ft:added the init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from flask_bootstrap import Bootstrap
 from flask import Flask
 from .config import DevConfig
 
@@ -7,4 +8,7 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(DevConfig)
 app.config.from_pyfile("config.py")
 
+bootstrap = Bootstrap(app)
+
 from app import views
+from app import error
